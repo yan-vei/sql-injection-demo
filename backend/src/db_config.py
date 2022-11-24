87ecc33c0384b3fbbef599c52a6fcc46cd4ab603
Yana Veitsman <veytsman.karaganda@gmail.com>
2022-11-24 15:50:23 +0200

    allow raw sql queries

---
+++ backend/src/db_config.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+
+from sqlalchemy import create_engine
+
+db_url = 'localhost:5432'
+db_name = 'sql-injection-demo'
+db_user = 'postgres'
+db_password = 'sq1-injection-dem0'
+engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_url}/{db_name}')
+
