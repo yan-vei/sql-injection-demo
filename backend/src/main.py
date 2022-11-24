3fcfb6206718dd15c8ffa49571f286c3ba9a1a4d
Yana Veitsman <veytsman.karaganda@gmail.com>
2022-11-24 22:27:59 +0200

    add registration and login for users

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,8 +1,39 @@
-from flask import Flask
+from flask import Flask, request, make_response, jsonify
 from backend.src.db_config import engine
+from flask_cors import CORS
+from controllers import user
 
 
 app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/login", methods=['POST'])
+def login():
+  username = request.args.get('username')
+  password = request.args.get('password')
+  user_login = user.login_user(username, password)
+
+  if user_login:
+      return make_response(jsonify(user_login), 200)
+  else:
+      return make_response(jsonify({"message":"User does not exist."}), 400)
+
+
+
+@app.route("/register", methods=['POST'])
+def register():
+  username = request.args.get('username')
+  password = request.args.get('password')
+  first_name = request.args.get('first_name')
+  last_name = request.args.get('last_name')
+  email = request.args.get('email')
+  registered_user = user.register_user(username, password, email, first_name, last_name)
+
+  if 'email' not in registered_user:
+      return make_response(jsonify({"message": registered_user}), 400)
+  else:
+      return make_response(jsonify(registered_user), 200)
 
 
 if __name__ == '__main__':
@@ -19,14 +50,5 @@ if __name__ == '__main__':
         );
         '''
     )
-
-    engine.execute(
-        '''
-        INSERT INTO users(email, first_name, last_name, passwd, username, admin) VALUES 
-        ('john.doe@zmail.com', 'John', 'Doe', 'john@123', 'johnd', FALSE);
-        '''
-    )
-    for record in engine.execute('SELECT * FROM users;'):
-        print(record)
 
     app.run()
